fa95876c9e2e86184ee107e0b050a5f4dff883d8
srubin <srubin@deloitte.com>
2017-02-09 19:57:17 -0500

    added bg only to the references of elements, also optimized performance with one call for the entire element array

--- routes/jsonschema/GET.js
+++ routes/jsonschema/GET.js
@@ -26,7 +26,7 @@ module.exports = function jsonschema(req, res, next) {
 
   // TODO: Move node async with callbacks to a promise based asynchronous handling
   // For a given list of element names return their full document object and their children as derived from the type. Convert to JSON Schema format.
-  function getElementObjects(elements, cb) {
+  function getElementObjects(elements, cb, parent) {
 
     // Filter out any elements who have already been added to the schema. Prevents infinite recursion caused by circular references in the data. 
     elements = elements.filter( (element) => {
@@ -42,8 +42,16 @@ module.exports = function jsonschema(req, res, next) {
         return elArrItem.isBG;
       });
 
+
       // For valid elements, asynchronously make the request to recurse each. When done processing all requests in array, return the response; or call up the tree.
       async.each(elArr, (el, callback) => {
+
+        // Add this BG element to the parent now that we have confirmed that is in fact BG
+        if (parent) {
+          schemaExport.properties[parent].properties[el.name] = {
+            "$ref": "#/properties/" + el.name
+          };
+        }
 
         // Start building out the JSON schema for the current element.
         let elSchema = {};
@@ -57,14 +65,9 @@ module.exports = function jsonschema(req, res, next) {
               elSchema.type = "object";
               elSchema.properties = {};
 
-              elType.elements.forEach( (element) => {
-                elSchema.properties[element] = {
-                  "$ref": "#/properties/" + element
-                };
-              });
 
               schemaExport.properties[el.name] = elSchema;
-              getElementObjects(elType.elements, callback);
+              getElementObjects(elType.elements, callback, el.name);
           
             } else {
               elSchema.type = elType.name;
