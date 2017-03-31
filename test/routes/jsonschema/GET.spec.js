216e3265ec21a744657e7f69a3576c928d9d4820
srubin <srubin@deloitte.com>
2017-03-31 15:33:18 -0400

    added init unit testing

---
+++ test/routes/jsonschema/GET.spec.js
@@ -0,0 +1,47 @@
+'use strict';
+let expect = require('chai').expect,
+	sinon = require('sinon'),
+	locreq = require('locreq')(__dirname);
+
+locreq('index');
+
+describe('jsonschema: GET', function() {
+
+	let jsonschemaGET,
+		next_spy,
+		req,
+		res,
+		sandbox;
+	before(function() {
+		jsonschemaGET = locreq('routes/jsonschema/GET');
+	});
+
+	beforeEach(function() {
+		sandbox = sinon.sandbox.create();
+		next_spy = sandbox.spy();
+
+		req = {};
+		res = {
+			json: sandbox.spy()
+		};
+	});
+
+	afterEach(function() {
+		sandbox.restore();
+	});
+
+	it('should return data', function(done) {
+		jsonschemaGET(req, res, next_spy).then(function() {
+			expect(res.json.calledWith('foo')).to.be.true;
+			done();
+		});
+	});
+
+	it('should pass errors to the error handler', function(done) {
+		jsonschemaGET(req, res, next_spy).then(function() {
+			expect(next_spy.calledOnce).to.be.true;
+			done();
+		});
+	});
+
+});
